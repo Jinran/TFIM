b17ce419a3864a875ce182ae7ad643eb8901bc0b
然 金 <dav_jin@163.com>
2016-07-15 17:34:39 +0800

    添加项目文件。

---
+++ TFIM/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,63 @@
+﻿angular.module('starter.controllers', [])
+
+.controller('DashCtrl', ['$scope', 'chat', function ($scope, chat) {
+    /*document.addEventListener('jmessage.onReceiveTextMessage', function () {
+        var msg = window.JMessage.textMessage;
+        alert('接收消息成功：' + msg);
+    }, false);*/
+
+    $scope.$on('jmessage.onReceiveTextMessage', function () {
+        var msg = window.JMessage.textMessage;
+        alert('接收消息成功：' + msg);
+    });
+    $scope.username = '';
+    $scope.userpassword = '';
+    $scope.text = '';
+    $scope.login = function (username, userpassword) {
+        chat.login(username, userpassword, function () {
+            alert('登录成功');
+        }, function (errorStr) {
+            alert(errorStr);
+        });
+    };
+        
+    $scope.register = function (username, userpassword) {
+        chat.register(username, userpassword, function () {
+            alert("注册成功");
+        }, function (errorStr) {
+            alert(errorStr);
+        })
+    };
+    $scope.sendSingleTextMessage = function (text) {
+        chat.sendSingleTextMessage('jinran', text, function (response) {
+            //alert('发送消息成功');
+        }, function (errorStr) {
+            alert('发送消息失败：' + errorStr);
+        })
+    };
+}])
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ TFIM/www/js/chatService.js
@@ -0,0 +1,35 @@
+﻿angular.module('chatService', [])
+//JMessage插件封装service
+.service('chat', function () {
+    var appkey = '2e675ac288e30e8109bda11e';
+
+    //测试service
+    this.testFun = function () {
+        return "testFun";
+    };
+
+    //JM注册
+    this.register = function (username, password, successCallback, errorCallback) {
+        window.JMessage.register(username, password, successCallback, errorCallback);
+    };
+
+    //JM登录
+    this.login = function (username, password, successCallback, errorCallback) {
+        window.JMessage.login(username, password, successCallback, errorCallback);
+    };
+
+    //JM登出
+    this.logout = function () {
+        window.JMessage.logout(successCallback, errorCallback)
+    };
+
+    //JM获取用户信息
+    this.getUserInfo = function (username, successCallback, errorCallback) {
+        window.JMessage.getUserInfo(username, appkey, successCallback, errorCallback)
+    };
+
+    //JM发送一条单聊文本消息
+    this.sendSingleTextMessage = function (username, text, successCallback, errorCallback) {
+        window.JMessage.sendSingleTextMessage(username, text, appkey, successCallback, errorCallback);
+    };
+})
